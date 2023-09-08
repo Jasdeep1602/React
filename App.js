bc5bf40805fb8bb383d7b768aa6058397ccaf565
Jasdeep Singh <singhjasdeep1602@gmail.com>
2023-09-08 11:46:10 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,21 @@
+//    <div id='parent'>
+//     <div id='child'>
+//         <h1>I am h1</h1>
+//         <h2>I am h2</h2>
+//     </div>
+// </div>
+
+// ReactElement(object) => HTML(browser Understands)
+// render converts parent to html
+
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, 'I am h1'),
+    React.createElement('h2', {}, 'I am h2'),
+  ])
+)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(parent)
