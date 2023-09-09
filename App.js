9c16cf3764c560bacde582322513869fd805d102
Jasdeep Singh <singhjasdeep1602@gmail.com>
2023-09-09 19:40:58 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//    <div id='parent'>
-//     <div id='child'>
-//         <h1>I am h1</h1>
-//         <h2>I am h2</h2>
-//     </div>
-// </div>
 
-// ReactElement(object) => HTML(browser Understands)
-// render converts parent to html
+const heading = <h1 className='heading'>Hello</h1>
 
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'I am h1'),
-    React.createElement('h2', {}, 'I am h2'),
-  ])
-)
+const HeadingComp = () => <h1 className='heading'>Hello World</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(parent)
+root.render([heading, <HeadingComp />])
